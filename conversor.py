d6fa995b26fec451c5c9d9649745f03103b791c1
Gabriel Murillo Medina <gamurigm@gmail.com>
2020-11-18 14:08:32 -0500

    Conversor implementando funciones

--- conversor.py
+++ conversor.py
@@ -1,7 +1,38 @@
-pesos = float(input("Pesos: "))
-valor_dolar = 3875
-dolares = pesos / valor_dolar
-dolares = round(dolares, 2)
-dolares = str(dolares)
+# pesos = float(input("Pesos: "))
+# valor_dolar = 3875
+# dolares = pesos / valor_dolar
+# dolares = round(dolares, 2)
+# dolares = str(dolares)
+# print("Tienes $" + dolares + "dólares")
 
-print("Tienes $" + dolares + "dólares")
+def conversor(pesos, tipo_cambio):
+    pesos = float(input("Pesos " + pesos + ":"))
+    valor_dolar = 3875
+    dolares = pesos / valor_dolar
+    dolares = round(dolares, 2)
+    dolares = str(dolares)
+    print("Tienes $" + dolares + " dólares")
+
+
+menu = """
+Bienvenido al conversor de monedas 
+
+Opción 1. - Pesos Colombianos
+Opción 2. - Pesos Argentinos
+Opción 3. - Pesos Uruguayos
+
+Elije una opción: """
+
+opcion = int(input(menu))
+
+if opcion == 1:
+    conversor("colombianos", 3875)
+    
+elif opcion ==2:  
+    conversor("argentinos", 195)
+
+elif opcion == 3:
+    conversor("uruguayos", 24)
+
+else:
+    print("Ingrese una opción correcta!")
---
+++ mayor de edad.py
@@ -0,0 +1,6 @@
+edad = int(input("Escribe tu edad: "))
+if edad > 17: 
+    print("Es mayor de edad")
+else:
+    print("Es menor de edad")
+
