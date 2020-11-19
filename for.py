7a62f7e830c6d31ac9a6446c24d2dc06dcd54194
Gabriel Murillo Medina <gamurigm@gmail.com>
2020-11-19 18:19:59 -0500

    ciclos...

--- for.py
+++ for.py
@@ -1,8 +1,11 @@
 
 
 contador = 1
-print(contador)
+# print(contador)
 
-while contador < 1000:
-    contador = contador + 1
+# while contador < 1000:
+#     contador = contador + 1
+#     print(contador)
+
+for contador in range(1, 1001):
     print(contador)
